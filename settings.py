25dbc245d21b2d70b948e02882e17866183c658d
MattM <mattmerrica@gmail.com>
2022-04-02 12:45:01 -0700

    setting up files
    setting up files

---
+++ settings.py
@@ -0,0 +1,22 @@
+level_map = [
+'                                                                                                                                                                                                                                 ',
+'                                                                                                                                                                                                                                 ',
+'                                                                                                                                                                                                                                 ',
+'                                                                                                                                                                                                                                  ',
+'                                                                                                                                                                                                                                  ',
+'                                                                                G G                                                                                                                                                ',
+'                      ?                                                         BBBBBBBB   BBB?             M            BBB    B??B                                                         RR                                     ',
+'                                                                                                                                                                                            RRR                                     '
+'                                                                                                                                                                                           RRRR                                     '
+'                                                               I                                                                                                                          RRRRR                                     '
+'                ?   BMB?B                                                    BMB              L    BS    ?  ?  ?      B          BB      R  R          RR  R             BB?B            RRRRRR                                     '
+'                                                                                                                                        RR  RR        RRR  RR                           RRRRRRR                                     '
+'                                                                                                          K                            RRR  RRR      RRRR  RRR                         RRRRRRRR                              ',
+'                      G                 G           G G                                         G G              G  G       G G G G   RRRR  RRRR    RRRRR  RRRR                G G    RRRRRRRRR        R                     ',
+'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  XXXXXXXXXXXXXXX   XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  XXXXXXXXXXXXXXX   XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+
+tile_size = 64
+screen_width = 1200
+#screen_height = 1024
+screen_height = len(level_map) * tile_size
