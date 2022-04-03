ad492e096ce40ac397baef0d85133a7bc43b1ca8
MattM <mattmerrica@gmail.com>
2022-04-02 22:40:10 -0700

    fix levels

--- Level.py
+++ Level.py
@@ -59,27 +59,6 @@ class Level:
         else:
             self.world_shift = 0
             player.speed = 8
-    #def horizontal_movement_collision(self):
-     #   player = self.player.sprite
-    #    player.rect.x += player.direction.x * player.speed
-
-     #   for sprite in self.tiles.sprites():
-      #      if sprite.rect.colliderect(player.rect):
-     #           if player.direction.x < 0:
-     #               player.rect.left = sprite.rect.right
-      #          elif player.direction.x > 0:
-    #                player.rect.right = sprite.rect.left
-    #def vertical_movement_collision(self):
-    #    player = self.player.sprite
-    #    player.apply_gravity()
-     #   for sprite in self.tiles.sprites():
-     #       if sprite.rect.colliderect(player.rect):
-     #           if player.direction.y > 0:
-     #               player.rect.bottom = sprite.rect.top
-     #               player.direction.y = 0
-    # #           elif player.direction.y < 0:
-     #               player.rect.top = sprite.rect.bottom
-    #                player.direction.y = 0
     def horizontal_movement_collision(self):
         player = self.player.sprite
         player.rect.x += player.direction.x * player.speed
@@ -88,28 +67,20 @@ class Level:
             if sprite.rect.colliderect(player.rect):
                 if player.direction.x < 0:
                     player.rect.left = sprite.rect.right
-                    player.on_left = True
-                    self.current_x = player.rect.left
                 elif player.direction.x > 0:
                     player.rect.right = sprite.rect.left
-                    player.on_right = True
-                    self.current_x = player.rect.right
-
     def vertical_movement_collision(self):
         player = self.player.sprite
         player.apply_gravity()
-
+        self.tiles.apply_gravity()
         for sprite in self.tiles.sprites():
             if sprite.rect.colliderect(player.rect):
                 if player.direction.y > 0:
                     player.rect.bottom = sprite.rect.top
                     player.direction.y = 0
-                    player.on_ground = True
                 elif player.direction.y < 0:
                     player.rect.top = sprite.rect.bottom
                     player.direction.y = 0
-                    player.on_ceiling = True
-
     def run(self) :
         #tiles
         self.tiles.update(self.world_shift)
